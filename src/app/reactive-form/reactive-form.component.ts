ac15cd0c9dbc4b25ae96ce180f867f0400ed341c
Dhiraj <jireldhiraj123@gmail.com>
2023-12-16 02:47:18 -0800

    generate header, form component

---
+++ src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './reactive-form.component.html',
+  styleUrl: './reactive-form.component.scss'
+})
+export class ReactiveFormComponent {
+
+}
---
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-driven-form.component.html',
+  styleUrl: './template-driven-form.component.scss'
+})
+export class TemplateDrivenFormComponent {
+
+}
